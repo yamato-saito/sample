287aac8c7c45fc0da790e3e6e43ed5a6ef30d8c1
yamato-saito <yamato970428@docomo.ne.jp>
2022-10-18 13:26:05 +0900

    組み込み型(数値)

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
 func main() {
-
-	t := time.Now().UnixNano()
-	rand.Seed(t)
-	s := rand.Intn(6) + 1
-	//fmt.Printf("%d", s)
-	switch s {
-	case 1:
-		fmt.Printf("凶")
-	case 2, 3:
-		fmt.Printf("吉")
-	case 4, 5:
-		fmt.Printf("中吉")
-	case 6:
-		fmt.Printf("大吉")
+	sum := 5 + 6 + 3
+	var avg float64 = float64(sum) / 3
+	if avg > 4.5 {
+		println("good")
 	}
-
 }

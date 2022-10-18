593dfb096376d7cf0fcb46a8346569ffe7bbb476
yamato-saito <yamato970428@docomo.ne.jp>
2022-10-18 12:25:40 +0900

    奇数と偶数

--- main.go
+++ main.go
@@ -3,8 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var n int = 100
-	const m int = 200
-	fmt.Print("値段>")
-	fmt.Printf("%d円%d円引き\n", n, m)
+	//var n int = 100
+	//const m int = 200
+
+	for i := 0; i <= 100; i = i + 1 {
+		fmt.Printf("%d-", i)
+		if i%2 == 1 { //奇数
+			fmt.Printf("奇数\n")
+		} else { //偶数
+			fmt.Printf("偶数\n")
+		}
+	}
+	//fmt.Print("値段>")
+	//fmt.Printf("%d円%d円引き\n", n, m)
 }

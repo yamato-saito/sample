023ff58b90d8f6c56875de56e058d7b0a052177a
yamato-saito <yamato970428@docomo.ne.jp>
2022-10-18 12:50:47 +0900

    おみくじ

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	//var n int = 100
-	//const m int = 200
 
-	for i := 0; i <= 100; i = i + 1 {
-		fmt.Printf("%d-", i)
-		if i%2 == 1 { //奇数
-			fmt.Printf("奇数\n")
-		} else { //偶数
-			fmt.Printf("偶数\n")
-		}
+	t := time.Now().UnixNano()
+	rand.Seed(t)
+	s := rand.Intn(6) + 1
+	//fmt.Printf("%d", s)
+	switch s {
+	case 1:
+		fmt.Printf("凶")
+	case 2, 3:
+		fmt.Printf("吉")
+	case 4, 5:
+		fmt.Printf("中吉")
+	case 6:
+		fmt.Printf("大吉")
 	}
-	//fmt.Print("値段>")
-	//fmt.Printf("%d円%d円引き\n", n, m)
+
 }

e7f5803a4591e3613879262ddf85bebd9bac241b
yamato-saito <yamato970428@docomo.ne.jp>
2022-10-19 20:01:58 +0900

    catコマンドを作ろう

--- main.go
+++ main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
 )
 
+// 設定される変数のポインタを取得
+// var msg = flag.String("msg", "デフォルト値", "説明")
+
+var n = flag.Bool("n", false, "bool flag")
+
 func main() {
+	//fmt.Println(os.Args[1])
+	// ここで実際に設定される
+	flag.Parse()
+	//fmt.Println(strings.Repeat(_, n))
+	i := 0
 
-	fmt.Println(os.Args)
+	// 読み込み用にファイルを開く
 
+	for _, fn := range flag.Args() {
+
+		sf, err := os.Open(fn)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "エラー")
+			os.Exit(1)
+		}
+		// 関数終了時に閉じる
+		defer sf.Close()
+
+		scanner := bufio.NewScanner(sf)
+
+		for scanner.Scan() {
+
+			//1行分を出力する
+			if *n {
+				i++
+				fmt.Printf("%d: ", i)
+			}
+			fmt.Println(scanner.Text())
+		}
+	}
 }
